408adf352fec250c00e03e3289037ab8c16201d5
Michał Urbańczyk <urbanczyka3k@gmail.com>
2022-05-21 19:40:08 +0200

    python jmdict_merger visual refactor and unnecessary command removal

--- utils/database_entries_creator/jmdict_enamdict_merger/main.py
+++ utils/database_entries_creator/jmdict_enamdict_merger/main.py
@@ -2,8 +2,6 @@ import json
 import xml.etree.cElementTree as ET
 from dataclasses import dataclass
 
-
-
 @dataclass
 class LanguageMeanings:
     language: str
@@ -21,17 +19,13 @@ class Entry:
         self.meanings = list()
 
     def parse_meaning(self, language, meaning):
-        for languageMeaning in self.meanings:
-            if languageMeaning.language == language:
-                languageMeaning.append_meaning(meaning)
+        for language_meaning in self.meanings:
+            if language_meaning.language == language:
+                language_meaning.append_meaning(meaning)
                 break
         else:
             self.meanings.append(LanguageMeanings(language=language, meanings=[meaning]))
 
-    
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
 
 class EntryJSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -43,35 +37,34 @@ class EntryJSONEncoder(json.JSONEncoder):
 class JMDictProcessor:
     def __init__(self, file) -> None:
         self.file = file
-        
-    def XML_to_dict(self,) -> list():
+
+    def xml_to_dict(self,) -> list():
         result = list()
         xml_file = ET.parse(self.file)
         for entry in xml_file.getroot():
-            entryId = entry.find('ent_seq').text
-            resultEntry = Entry()
+            result_entry = Entry()
             for kanji in entry.iter('keb'):
-                resultEntry.kanjis.append(kanji.text)
+                result_entry.kanjis.append(kanji.text)
 
             for reading in entry.iter('reb'):
-                resultEntry.readings.append(reading.text)
+                result_entry.readings.append(reading.text)
 
             for sense in entry.iter('sense'):
                 for part in sense:
                     if part.tag=="pos":
-                        resultEntry.part_of_speech.add(part.text)
+                        result_entry.part_of_speech.add(part.text)
                     if part.tag == "gloss":
-                        if part.text != None:
-                            resultEntry.parse_meaning(part.get('{http://www.w3.org/XML/1998/namespace}lang'), part.text)
+                        if part.text is not None:
+                            result_entry.parse_meaning(part.get('{http://www.w3.org/XML/1998/namespace}lang'), part.text)
 
-            result.append(resultEntry)
+            result.append(result_entry)
         return result
 
 class JMEdictProcessor:
     def __init__(self, file) -> None:
         self.file = file
 
-    def XML_to_dict(self,) -> list():
+    def xml_to_dict(self,) -> list():
         result = list()
         xml_file = ET.parse(self.file)
         for entry in xml_file.getroot():
@@ -87,7 +80,7 @@ class JMEdictProcessor:
                     if part.tag=="name_type":
                         result_entry.part_of_speech.add(part.text)
                     if part.tag == "trans_det":
-                        if part.text != None:
+                        if part.text is not None:
                             result_entry.parse_meaning('eng', part.text)
 
             result.append(result_entry)
@@ -97,17 +90,16 @@ if __name__ == "__main__":
     JMdictFile = open('inputFiles/JMdict/JMdict', 'r', encoding="utf-8")
     JMnedictFile = open('inputFiles/JMdict/JMnedict.xml', 'r', encoding="utf-8")
     jmdictProcessor = JMDictProcessor(JMdictFile)
-    JMdict = jmdictProcessor.XML_to_dict()
+    JMdict = jmdictProcessor.xml_to_dict()
 
     jmEdictProcessor = JMEdictProcessor(JMnedictFile)
-    JMnedict = jmEdictProcessor.XML_to_dict()
+    JMnedict = jmEdictProcessor.xml_to_dict()
 
     mergedDicts = JMdict + JMnedict
 
-    out_file = open("mergedDicts.json", "w", encoding="utf-8")
+    out_file = open("partiallyProcessedFiles/JMdict/mergedDicts.json", "w", encoding="utf-8")
     json.dump(mergedDicts, out_file, cls=EntryJSONEncoder, sort_keys=True, indent=4)
 
     out_file.close()
 
     # print(get_size(result))
-    pass
