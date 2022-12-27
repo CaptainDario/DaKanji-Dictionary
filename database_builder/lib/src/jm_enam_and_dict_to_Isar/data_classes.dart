3a53416643881563b424c6f73e09012bd66d43ac
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-27 14:56:09 +0100

    updated docs

--- database_builder/lib/src/jm_enam_and_dict_to_Isar/data_classes.dart
+++ database_builder/lib/src/jm_enam_and_dict_to_Isar/data_classes.dart
@@ -1,12 +1,14 @@
 import 'package:isar/isar.dart';
 part 'data_classes.g.dart';
 
+/// Convenience class to bundle the meanings of `JMdict` / `JMNEdict` entries
+/// and their translations (possibly different languages)
 @embedded
 class LanguageMeanings {
   String? language;
   List<String>? meanings;
 
-  LanguageMeanings({String? this.language, List<String>? this.meanings});
+  LanguageMeanings({this.language, this.meanings});
 
   @override
   String toString() {
@@ -18,14 +20,20 @@ class LanguageMeanings {
   }
 }
 
+/// Class to store the most important bits of a JMNEdict database entry. 
 @Collection(accessor: "jmnedict")
 class JMNEdict {
+  /// A unique ID identifying this JMDict entry
   Id id = Isar.autoIncrement;
+  /// A list containing different versions how to write this entry using Kanji
   @Index(type: IndexType.value)
   List<String> kanjis;
+  /// A list containing different versions how to read this entry
   @Index(type: IndexType.value)
   List<String> readings;
+  /// The part of speech elements of this entry
   List<String> partOfSpeech;
+  /// The meanings of this entry and their translations
   List<LanguageMeanings> meanings = <LanguageMeanings>[];
 
   JMNEdict(
@@ -40,15 +48,22 @@ class JMNEdict {
   }
 }
 
+/// Class to store the most important bits of a JMDict database entry. 
 @Collection(accessor: "jmdict")
 class JMdict {
+  /// A unique ID identifying this JMDict entry
   Id id = Isar.autoIncrement;
+  /// A list containing different versions how to write this entry using Kanji
   @Index(type: IndexType.value)
   List<String> kanjis;
+  /// A list containing different versions how to read this entry
   @Index(type: IndexType.value)
   List<String> readings;
+  /// The part of speech elements of this entry
   List<String> partOfSpeech;
+  /// The meanings of this entry and their translations
   List<LanguageMeanings> meanings = <LanguageMeanings>[];
+  /// The frequency of this entry (follows a zipf distribution)
   float frequency;
 
   JMdict(
