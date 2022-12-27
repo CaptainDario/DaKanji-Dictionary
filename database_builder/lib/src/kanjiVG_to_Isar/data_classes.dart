bdd31092f826c07876363d679b434d5ecd960b29
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-27 10:13:32 +0100

    use character as index

--- database_builder/lib/src/kanjiVG_to_Isar/data_classes.dart
+++ database_builder/lib/src/kanjiVG_to_Isar/data_classes.dart
@@ -2,14 +2,30 @@ import 'package:isar/isar.dart';
 
 part 'data_classes.g.dart';
 
+
+
+/// Class to store the most important bits of a KanjiVG database entry. 
 @collection
 class KanjiSVG {
+  /// A unique id of this Kanji's entry in the database
   Id id = Isar.autoIncrement;
+  /// This entries Kanji character
+  @Index(type: IndexType.hash)
   String character;
+  /// The SVG data to of this Kanji
   String svg;
-  @Index(type: IndexType.value)
+  @Index(type: IndexType.hash)
+  /// Number of strokes of this Kanji
   short strokes;
+  /// All radicals used in this Kanji
   List<String> radicals;
 
-  KanjiSVG({required this.character, required this.svg, required this.strokes, required this.radicals});
+  KanjiSVG(
+    {
+      required this.character,
+      required this.svg,
+      required this.strokes,
+      required this.radicals
+    }
+  );
 }
