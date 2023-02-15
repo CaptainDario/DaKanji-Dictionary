8a2d0520da06fe2dee7fd7615dbdeb81d6725095
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-02-15 11:55:26 +0100

    added: main

--- database_builder/lib/src/tatoeba_to_isar/parse.py
+++ database_builder/lib/src/tatoeba_to_isar/parse.py
@@ -2,31 +2,30 @@ import MeCab;
 import ipadic;
 import sys
 import json
-from tqdm import tqdm
 
 
 
-# if(__name__ == "__main__"):
-print("Adding mecab processing to Japanese sentences")
+if(__name__ == "__main__"):
+    print("Adding mecab processing to Japanese sentences")
 
-# read json
-with open(sys.argv[1], encoding="utf8") as f:
-    js = json.loads(f.read())
+    # read json
+    with open(sys.argv[1], encoding="utf8") as f:
+        js = json.loads(f.read())
 
-# parse all japanese sentences using mecab
-tagger = MeCab.Tagger(ipadic.MECAB_ARGS)
-surfaces, tokens = [], []
-for key, value in tqdm(js.items()):
-    parsedValue = tagger.parse(str(value)).split("\n")
-    js[key] = [value, [], []]
-    for mecabOutItems in parsedValue:
-        mecabOutItems = mecabOutItems.split("\t")
+    # parse all japanese sentences using mecab
+    tagger = MeCab.Tagger(ipadic.MECAB_ARGS)
+    surfaces, tokens = [], []
+    for key, value in js.items():
+        parsedValue = tagger.parse(str(value)).split("\n")
+        js[key] = [value, [], []]
+        for mecabOutItems in parsedValue:
+            mecabOutItems = mecabOutItems.split("\t")
 
-        if(len(mecabOutItems) != 1):
-            js[key][1].append(mecabOutItems[0])
-            js[key][2].append(mecabOutItems[1].split(","))
+            if(len(mecabOutItems) != 1):
+                js[key][1].append(mecabOutItems[0])
+                js[key][2].append(mecabOutItems[1].split(","))
 
-with open(sys.argv[1].replace("jpn", "jpn_mecab"), mode="w+", encoding="utf8") as f:
-    js = f.write(json.dumps(js, ensure_ascii=False))
+    with open(sys.argv[1].replace("jpn", "jpn_mecab"), mode="w+", encoding="utf8") as f:
+        js = f.write(json.dumps(js, ensure_ascii=False))
 
-print("Finished mecab processing to Japanese sentences")
+    print("Finished mecab processing to Japanese sentences")
