2fc97c5e00bf398ce84df2e059180dc111963a77
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-06-26 20:35:40 +0200

    added docs

--- database_builder/lib/src/kanjiVG_to_Isar/kanjiVG_to_Isar.dart
+++ database_builder/lib/src/kanjiVG_to_Isar/kanjiVG_to_Isar.dart
@@ -3,10 +3,14 @@ import 'package:database_builder/database_builder.dart';
 import 'package:isar/isar.dart';
 import 'package:path/path.dart' as p;
 
+
+
+/// Extracts the stroke count from the given `fileContent` (kanjivg entry)
 short getStrokeCount(String fileContent){
   return RegExp(r'<text transform="matrix.*<\/text>').allMatches(fileContent).length;
 }
 
+/// Extracs all radicals from the given `fileContent` (kanjivg entry)
 List<String> getAllRadicals(String fileContent){
   List<String> radicals = <String>[];
   for (final Match m in RegExp(r'<g.* kvg:element="(.)".* kvg:radical="(.)*">').allMatches(fileContent)) {
@@ -15,6 +19,7 @@ List<String> getAllRadicals(String fileContent){
   return radicals;
 }
 
+/// Converts the kanjiVG database to an isar instance
 Future<bool> kanjiVGToIsar(Isar isar) async {
 // Future<bool> main() async{
   var dbName = 'kanji_SVG';
